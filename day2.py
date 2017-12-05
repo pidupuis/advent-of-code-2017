f3dfe1fb661b65214e5564e7dda356b03f7c292a
Pierre Dupuis 井磊 <pidupuis@users.noreply.github.com>
2017-12-05 14:54:34 +0100

    feat: solve day 2

---
+++ day2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import pandas as pd
+import itertools
+
+df = pd.read_csv('day2.csv', header=None)
+
+# Part 1
+print(sum(df.apply(lambda row: row.max() - row.min(), axis=1)))
+
+
+# Part 2
+print(sum(df.apply(lambda row: [int(a / b) for (a, b) in list(
+        itertools.combinations(row.sort_values(ascending=False), 2)
+    ) if a % b == 0][0], axis=1)))
